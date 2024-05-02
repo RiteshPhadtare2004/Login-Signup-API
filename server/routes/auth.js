8246c59095ea6d145a3bffab3da93082743d7d29
Ritesh Phadtare <riteshphadtare12.02.2004@gmail.com>
2024-05-02 17:42:44 +0530

    added put, get, update requests

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -63,4 +63,53 @@ router.post('/login', async (req, res) =>{
     }
 });
 
+router.get('/home',async (req, res) => {
+    try {
+        const user = await User.find()
+        res.json(user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: 'Server Error'})
+    }
+})
+
+router.get('/profile/:id', async (req,res) =>{
+    try {
+        const user = await User.findById(req.params.id)
+        if(!user){
+            return res.status(404).json({message:'User not found'})
+        }else{
+            res.json(user)
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: 'Server Error'})
+    }
+})
+
+router.put('/profile/update/:id', async (req,res) =>{
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.id, req.body,{new: true})
+        if(!updateUser){
+            return res.status(404).json({message: 'User not found'})
+        }
+        res.json(updateUser)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:'Server Error'})
+    }
+})
+
+router.delete('/delete/:id', async (req,res) =>{
+    try {
+        const deleteUser = await User.findByIdAndDelete(req.params.id)
+        if(!deleteUser){
+            return res.status(404).json({message:'User not found'})
+        }
+        res.json({message:'User Deleted successfully'})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:'Server Error'})
+    }
+})
 module.exports = router;
